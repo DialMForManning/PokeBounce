78f7ff207b96abf59eb0fd92552cbc0dfb4b2dce
Brian Manning <btreelmanning@gmail.com>
2016-10-19 19:10:55 -0400

    rotating ball

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,24 @@ module.exports = {
   output: {
   	filename: "./lib/bundle.js"
   },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
   devtool: 'source-map',
 };
